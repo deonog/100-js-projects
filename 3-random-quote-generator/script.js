ca4a087e8df6c0615363bd0a8e1b2b4206d1bc47
Deon Ogbuokiri <deon.ogbuokiri@gmail.com>
2020-08-12 19:02:52 +0200

    random quotes generator

--- 3-random-quote-generator/script.js
+++ 3-random-quote-generator/script.js
@@ -1,6 +1,7 @@
 const quoteDOM = document.getElementById("quote");
 const authorDOM = document.getElementById("author");
 const btn = document.getElementById("button");
+const previousQuote = [];
 
 const quotes = [
   {
@@ -20,10 +21,20 @@ const quotes = [
 
 function generateQuote() {
   let currentQuote = quotes[Math.floor(Math.random() * quotes.length)];
-  console.log(currentQuote.quote);
-  console.log(currentQuote.author);
   quoteDOM.innerHTML = currentQuote.quote;
   authorDOM.innerHTML = currentQuote.author;
+
+  previousQuote.push(currentQuote);
+
+  if (previousQuote.length > 2) {
+    previousQuote.shift();
+
+    if (previousQuote[0] == previousQuote[1]) {
+    }
+    console.log(previousQuote[0]);
+    console.log(previousQuote[1]);
+    console.log("----------------");
+  }
 }
 
 btn.addEventListener("click", generateQuote);
