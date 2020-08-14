e2718a7116bd64810d9a26da5ccd8021a251eed0
Deon Ogbuokiri <deon.ogbuokiri@gmail.com>
2020-08-14 13:06:05 +0200

    counter colors

--- 5-counter-js/script.js
+++ 5-counter-js/script.js
@@ -3,12 +3,25 @@ const lowerBtn = document.getElementById("minus");
 const countDOM = document.getElementById("number");
 let count = 0;
 
+function checkColor() {
+  if (count < 0) {
+    countDOM.classList.add("red");
+  } else if (count === 0) {
+    countDOM.classList.remove("red");
+    countDOM.classList.remove("green");
+  } else {
+    countDOM.classList.add("green");
+  }
+}
+
 lowerBtn.addEventListener("click", function() {
   count--;
   countDOM.innerHTML = count;
+  checkColor();
 });
 
 addBtn.addEventListener("click", function() {
   count++;
   countDOM.innerHTML = count;
+  checkColor();
 });
