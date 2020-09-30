21c065bd998ec8411da7bc015fdeffeadad368f6
Deon Ogbuokiri <deon.ogbuokiri@gmail.com>
2020-09-30 16:55:51 +0200

    tip form js

--- 11-tip-form-js/script.js
+++ 11-tip-form-js/script.js
@@ -2,12 +2,12 @@
 
 const billInput = document.getElementById("price");
 const guestInput = document.getElementById("guest");
-const serviceOption = document.querySelectorAll("select option");
+const serviceOption = document.querySelector("select");
 const submitBtn = document.getElementById("submit-btn");
 const errorWrap = document.getElementById("error-wrapper");
 const billError = document.getElementById("bill-error");
 const guestError = document.getElementById("guest-error");
-const tipError = document.getElementById("tip-error");
+const serviceError = document.getElementById("tip-error");
 const resultWrap = document.getElementById("result");
 const tipResult = document.getElementById("tip-result");
 const guestResult = document.getElementById("guest-result");
@@ -17,15 +17,39 @@ const totalResult = document.getElementById("total-result");
 
 function validateForm(e) {
   e.preventDefault();
-  if (billInput.value == "" && guestInput.value <= 0) {
-    alert("error");
+  if (
+    (guestInput.value <= 0 || guestInput.value == "") &&
+    billInput.value == ""
+  ) {
+    showError();
   } else {
     calculate();
   }
 }
 
+function showError() {
+  errorWrap.style.display = "block";
+  if (guestInput.value == "") {
+    guestError.style.display = "block";
+  }
+
+  if (billInput.value == "") {
+    billError.style.display = "block";
+  }
+
+  if (serviceOption.value == "choose") {
+    serviceError.style.display = "block";
+  }
+}
+
 function calculate() {
-  alert("working");
+  errorWrap.style.display = "none";
+  const bill = billInput.value;
+  const guest = guestInput.value;
+  const service = serviceOption.value;
+  console.log(bill);
+  console.log(guest);
+  console.log(service);
 }
 
 // Event Listeners
